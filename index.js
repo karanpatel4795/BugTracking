9c5472381e7476347cd6c37de8bad8a992e374ce
karanpatel4795 <62552903+karanpatel4795@users.noreply.github.com>
2022-02-18 12:43:45 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require("express")
+const mongoose = require("mongoose")
+
 const sessionController = require("./controller/session-contoller")
+const roleController = require("./controller/role-controller")
+const userController = require("./controller/user-controller")
+const { urlencoded } = require("express")
+const UserModel = require("./Model/user-model")
+
 const app = express()
+
 //middle ware
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-
+mongoose.connect('mongodb://localhost:27017/BugTracking',function(err){
+    if(err){
+        console.log("Connection not Established");
+        console.log(err);
+    }
+    else{
+        console.log("Database Connected");
+    }
+})
 app.get("/",function(req,res){
     res.write("Welcome.....")
     res.end()
@@ -24,6 +40,20 @@ app.get("/signup",function(req,res){
 app.get("/login",sessionController.login)
 app.get("/signup",sessionController.signup)
 app.post("/saveuser",sessionController.saveuser)
+
+//role
+app.post("/roles",roleController.addRole)
+app.get("/roles",roleController.getAllRoles)
+app.delete("/roles",roleController.deleteRoles)
+app.put("/roles",roleController.updateRole)
+
+//user
+app.post("/users",userController.addUser)
+app.get("/users",userController.getAllUser)
+app.delete("/users/:userId",userController.deleteUser)
+app.put("/users",userController.updateUser)
+
+//server
 app.listen(3000,function(){
     console.log("server started on 3000");
 })
